98273e842edd8f99622c1cabff4c9f1ef4305baf
rezect <ilyapr1568@yandex.ru>
2024-12-22 12:30:29 +0300

    make import tg logo more readable

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import styles from '../styles/Footer.module.css'
+import tgLogo from '../../public/Logo.png'
 
 function Footer() {
   return (
     <>
       <h1 className={styles.footer_title}>Contacts</h1>
       <a href='https://t.me/rezect' target='_blank' rel='noreferrer' className={styles.footer_link}>
-        <img src='/public/Logo.png' className={styles.footer_logo}/>
+        <img src={tgLogo} className={styles.footer_logo}/>
       </a>
     </>
   )

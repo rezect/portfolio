805a492c7b2863a999fd890368fbba866479cfd2
rezect <ilyapr1568@yandex.ru>
2024-12-22 12:05:26 +0300

    change rotate to 45deg and make target='_blank' on link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <>
       <h1 className={styles.footer_title}>Contacts</h1>
-      <a href='https://t.me/rezect' className={styles.footer_link}>
+      <a href='https://t.me/rezect' target='_blank' rel='noreferrer' className={styles.footer_link}>
         <img src='/public/Logo.png' className={styles.footer_logo}/>
       </a>
     </>

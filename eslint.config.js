fcec97e0c48bf0ecd86ecd527d87ea8f24822a45
rezect <ilyapr1568@yandex.ru>
2024-12-22 12:38:33 +0300

    migrate to JS

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,29 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ['dist'], // Игнорируем папку с собранными файлами
+  },
+  {
+    extends: [js.configs.recommended], // Рекомендованные правила для JavaScript
+    files: ['**/*.{js,jsx}'], // Проверяем JS и JSX файлы
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2020, // Используем возможности ECMAScript 2020
+      globals: globals.browser, // Добавляем глобальные переменные браузера
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-hooks': reactHooks, // Подключаем плагин для React-хуков
+      'react-refresh': reactRefresh, // Подключаем плагин для React Refresh
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules, // Рекомендованные правила для хуков
       'react-refresh/only-export-components': [
-        'warn',
+        'warn', // Предупреждаем, если экспортируется что-то кроме компонентов
         { allowConstantExport: true },
       ],
     },
   },
-)
+];

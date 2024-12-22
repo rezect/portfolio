f95244cb62fbfab1cb522db8b8a1fdbcc117bdc9
rezect <ilyapr1568@yandex.ru>
2024-12-22 11:54:23 +0300

    edit App.tsx to use BD

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import Header from './components/Header'
 import Skills from './components/Skills'
 import Hobbies from './components/Hobbies'
 import About from './components/About'
+import BackgroundDecoration from './components/BackgroundDecoration'
 
 function App() {
   return (
     <>
       <Header/>
       <div className="main_section">
-        <Hobbies/>
-        <Skills/>
-        <About/>
+        <div className="cards_section">
+          <Hobbies/>
+          <Skills/>
+          <About/>
+        </div>
+        <BackgroundDecoration/>
       </div>
     </>
   )

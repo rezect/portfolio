ccfd8f1388ff22255d9bc1865367775debf947f5
rezect <ilyapr1568@yandex.ru>
2024-12-26 00:05:08 +0300

    Add github and hover for contacts

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import styles from '../styles/Footer.module.css'
-import tgLogo from '/Logo.png'
+import tgLogo from '/telegram-logo.png'
+import githubLogo from '/github-mark.png'
 
 function Footer() {
   return (
     <>
       <h1 className={styles.footer_title}>Contacts</h1>
-      <a href='https://t.me/rezect' target='_blank' rel='noreferrer' className={styles.footer_link}>
-        <img src={tgLogo} className={styles.footer_logo}/>
-      </a>
+      <div className={styles.footer}>
+        <a href='https://t.me/rezect' target='_blank' rel='noreferrer' className={styles.footer_link}>
+          <img src={tgLogo} className={styles.footer_logo}/>
+        </a>
+        <a href='https://github.com/rezect' target='_blank' rel='noreferrer' className={styles.footer_link}>
+          <img src={githubLogo} className={styles.footer_logo}/>
+        </a>
+      </div>
     </>
   )
 }
